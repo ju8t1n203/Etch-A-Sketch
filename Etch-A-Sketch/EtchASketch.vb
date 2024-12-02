257baa7ac617b83dac3d6eb1161e4420abaf4d1f
Justin Bell <Juxin203@gmail.com>
2024-12-02 08:40:24 -0700

    waveforms are doing things

--- Etch-A-Sketch/EtchASketch.vb
+++ Etch-A-Sketch/EtchASketch.vb
@@ -12,8 +12,7 @@
     End Function
 
     Sub MouseDraw(startX As Integer, startY As Integer, endX As Integer, endY As Integer)
-        Dim bmp As New Bitmap(DrawingPictureBox.Image)
-        Dim g As Graphics = Graphics.FromImage(bmp)
+        Dim g As Graphics = DrawingPictureBox.CreateGraphics()
         Dim pen As New Pen(penColor())
 
         g.DrawLine(pen, startX, startY, endX, endY)
@@ -37,8 +36,45 @@
         Next
     End Sub
 
+    Sub DrawSineWave()
+        Dim degrees As Double = Math.PI / 180
+        Dim peak As Integer = DrawingPictureBox.Height / 4
+        Dim currentY As Integer
+        Dim lastY As Integer = peak * Math.Sin(0) + 2 * peak
+        Dim lastX As Integer = 0
 
+        penColor(Color.Yellow)
+
+        For i As Integer = 1 To DrawingPictureBox.Width
+            currentY = peak * Math.Sin(i * degrees) + 2 * peak
+            MouseDraw(lastX, lastY, i, currentY)
+            lastX = i
+            lastY = currentY
+        Next
+
+    End Sub
+
+    'Buttons----------------------
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
+    End Sub
+
+    '-----------------------------
+    Private Sub DrawingPictureBox_MouseMove(sender As Object, e As MouseEventArgs) Handles DrawingPictureBox.MouseDown, DrawingPictureBox.MouseMove
+        Me.Text = $"({e.X.ToString}, {e.Y.ToString}) Button: {e.Button} Color: {penColor().Name}"
+        Static oldX%, oldY%
+
+        If e.Button = MouseButtons.Left Then
+            MouseDraw(oldX, oldY, e.X, e.Y)
+        End If
+
+        oldX = e.X
+        oldY = e.Y
+
+    End Sub
+
+    Private Sub WaveFormButton_Click(sender As Object, e As EventArgs) Handles WaveFormButton.Click
+        DrawDivisions()
+        DrawSineWave()
     End Sub
 End Class
