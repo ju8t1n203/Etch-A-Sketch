e9c91c4bb104c65b1462a0a5acf0e1890a8b2e08
Justin Bell <Juxin203@gmail.com>
2024-12-04 08:44:59 -0700

    all wave types added

--- Etch-A-Sketch/EtchASketch.vb
+++ Etch-A-Sketch/EtchASketch.vb
@@ -6,7 +6,6 @@
 
 Public Class EtchASketch
 
-
     Function penColor(Optional newColor As Color = Nothing) As Color
         Static _color As Color
 
@@ -41,6 +40,24 @@ Public Class EtchASketch
         Next
     End Sub
 
+    Sub DrawCosineWave()
+        Dim degrees As Double = Math.PI / 180
+        Dim peak As Integer = DrawingPictureBox.Height / 4
+        Dim currentY As Integer
+        Dim lastY As Integer = peak * Math.Cos(0) + 2 * peak
+        Dim lastX As Integer = 0
+
+        penColor(Color.Blue)
+
+        For i As Integer = 1 To DrawingPictureBox.Width
+            currentY = peak * Math.Cos(i * degrees) + 2 * peak
+            MouseDraw(lastX, lastY, i, currentY)
+            lastX = i
+            lastY = currentY
+        Next
+
+    End Sub
+
     Sub DrawSineWave()
         Dim degrees As Double = Math.PI / 180
         Dim peak As Integer = DrawingPictureBox.Height / 4
@@ -48,11 +65,36 @@ Public Class EtchASketch
         Dim lastY As Integer = peak * Math.Sin(0) + 2 * peak
         Dim lastX As Integer = 0
 
+        penColor(Color.Red)
+
         For i As Integer = 1 To DrawingPictureBox.Width
             currentY = peak * Math.Sin(i * degrees) + 2 * peak
             MouseDraw(lastX, lastY, i, currentY)
             lastX = i
             lastY = currentY
+        Next
+
+    End Sub
+
+    Sub DrawTangentWave()
+        Dim degrees As Double = Math.PI / 180
+        Dim peak As Integer = DrawingPictureBox.Height / 4
+        Dim currentY As Double
+        Dim lastY As Double = peak * Math.Tan(0) + 2 * peak
+        Dim lastX As Integer = 0
+
+        penColor(Color.Green)
+
+        For i As Integer = 1 To DrawingPictureBox.Width
+            currentY = peak * Math.Tan(i * degrees) + 2 * peak
+            If Math.Abs(currentY) <= 1000 Then
+                MouseDraw(lastX, lastY, i, currentY)
+                lastX = i
+                lastY = currentY
+            Else
+                lastX = i
+                lastY = peak * Math.Tan((i - 1) * degrees) + 2 * peak
+            End If
         Next
 
     End Sub
@@ -76,11 +118,13 @@ Public Class EtchASketch
     End Sub
 
     Private Sub WaveFormButton_Click(sender As Object, e As EventArgs) Handles WaveFormButton.Click
-        DrawSineWave()
         DrawDivisions()
+        DrawSineWave()
+        DrawCosineWave()
+        DrawTangentWave()
     End Sub
 
-    '-----------------------------
+    'Other Events------------------
     Private Sub GraphicsExample_Load(sender As Object, e As EventArgs) Handles Me.Load
         penColor(Color.Black)
         DrawingPictureBox.BackColor = Color.White
